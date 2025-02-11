575ef001dd9a92b5513f787122a23a0b870ceed9
Jesse Cusack <jesse.cusack@oregonstate.edu>
2025-02-11 19:32:17 +0800

    first attempt at viscous symmetric

--- src/pytg/solvers.py
+++ src/pytg/solvers.py
@@ -177,3 +177,100 @@ def viscous_taylor_goldstein(
     p = 1j * (X1 + X2 - X3) / k
 
     return freq, gr, cp, w.real, b.real, u.real, v.real, p.real
+
+
+@check_and_reshape_args(5)
+def viscous_symmetric(
+    z,
+    U,
+    V,
+    B,
+    By,
+    k,
+    l,
+    Kv,
+    Kb,
+    f = 1e-4,
+    vbc_top="rigid",
+    vbc_bot="rigid",
+    bbc_top="fixed",
+    bbc_bot="fixed",
+    acc=4,
+):
+    dz = ensure_equidistant_grid(z)
+    n = U.size
+    kh = np.sqrt(k**2 + l**2)  # Absolute horizontal wavenumber
+    # Velocity component parallel to the wave vector (k, l)
+    U = U * k / kh + V * l / kh
+
+    # Derivative matrices
+    D1 = fd.derivative_matrix(n, 1, acc, dz).toarray()
+    D2 = fd.derivative_matrix(n, 2, acc, dz).toarray()
+    D4 = fd.derivative_matrix(n, 4, acc, dz).toarray()
+
+    # Shear and buoyancy frequency.
+    Bz = D1 @ B
+    Uz = D1 @ U
+
+    # Create matrices with boundary conditions
+    DW1 = fd.derivative_matrix(n, 1, acc, dz, "fixed", "fixed").toarray()  # Impermeable
+    DQ1 = fd.derivative_matrix(n, 1, acc, dz, "rigid", "rigid").toarray()
+    # DB2 = fd.derivative_matrix(n, 1, acc, dz, "insulating", "insulating")
+
+    # Assemble matrices for eigenvalue computation
+    I = np.eye(n)  # noqa: E741
+    L = D2 - I * kh**2  # Laplacian
+    # LB = DB2 - I * kh**2
+    LL = D4 - 2 * D2 * kh**2 + I * kh**4  # Laplacian of laplacian
+
+    A = np.block([[I, np.zeros_like(L), np.zeros_like(L)],
+                  [np.zeros_like(L), L, np.zeros_like(L)], 
+                  [np.zeros_like(L), np.zeros_like(L), I]])
+
+
+    b11 = -1j*k*np.diag(np.squeeze(U))@L+ Kv*L
+    b12 = 1j*l*Uz + f*DW1
+    b13 = np.zeros_like(I)
+
+    b21 = -f*DQ1
+    b22 = (
+        -1j * k * np.diag(np.squeeze(U)) @ L
+        + Kv * LL
+    )
+    b23 = -I * kh**2
+
+    b31 = 1j*k / kh**2 * np.diag(np.squeeze(By))
+    b32 = 1j*l / kh**2 * np.diag(np.squeeze(Bz)) @ DW1 - np.diag(np.squeeze(Bz))
+    b33 = -1j * k * np.diag(np.squeeze(U)) + Kb * L
+
+    B = np.block([[b11, b12, b13], [b21, b22, b23], [b31, b32, b33]])
+
+    om, vec = eig(B, A)
+
+    isort = np.argsort(-om.imag)  # equivalent to sorting by cp
+    om = om[isort]
+    vec = vec[:, isort]
+    q = vec[:n, :]
+    w = vec[n:2*n, :]
+    b = vec[2*n:, :]
+
+    cp = -om.imag / k  # Phase speed
+    gr = om.real  # Growth rate
+    freq = om.imag  # Frequency
+
+    # d_dz = FinDiff(0, dz, 1, acc=acc)
+    # d2_dz2 = FinDiff(0, dz, 2, acc=acc)
+
+    # Uvec = 1j * d_dz(w) / k
+
+    # theta = np.arctan2(l, k)
+    # u = Uvec * np.cos(theta)
+    # v = Uvec * np.sin(theta)
+
+    # X1 = d_dz(U) * w
+    # X2 = (cp - U) * d_dz(w)
+    # X3 = 1j * Kv * (d2_dz2(d_dz(w)) - k * d_dz(w)) / k
+    # p = 1j * (X1 + X2 - X3) / k
+
+    # return freq, gr, cp, w.real, b.real, u.real, v.real, p.real
+    return freq, gr, cp, q.real, w.real, b.real
